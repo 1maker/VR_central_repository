c132e259068b6f202473d3e74f8ef5221900a70a
Shayakhmetov Airat <1maker0n@gmail.com>
2020-06-02 18:36:07 +0500

    Переименовал файл на 3.2.js и добавил тернарный оператор

--- 3.2.js
+++ 3.2.js
@@ -1,4 +1,5 @@
 function pow(x, n) {
+
   let result = 1;
 
   for (let i = 0; i < n; i++) {
@@ -11,9 +12,11 @@ function pow(x, n) {
 let x = prompt("x?", "");
 let n = prompt("n?", "");
 
-if (n < 0) {
+(n < 0) ? alert(`Степень ${n} не поддерживается, введите целую степень, большую 0`); :  alert( pow(x, n) );
+
+/*if (n < 0) {
   alert(`Степень ${n} не поддерживается,
     введите целую степень, большую 0`);
 } else {
   alert( pow(x, n) );
-}
+}*/
